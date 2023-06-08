497d97fa6d3a267a9102b487f8775264a1f0b891
pijusdas <pijusdas.dev@gmail.com>
2023-06-08 22:23:47 +0600

    added navbar

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Pages/Shared/Navbar";
+import Footer from "../Pages/Shared/Footer";
+
+ 
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet/>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Pages/Shared/Navbar.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => {
+    return (
+        <div className="navbar fixed z-10 bg-opacity-20 max-w-screen-xl bg-black text-white flex justify-between">
+            <div className=''>
+                <Link className=" text-2xl font-bold"><span className="text-yellow-600">Nota</span><span className="text-red-800"> Corda</span></Link>
+            </div>
+
+            <div className=" space-x-3 font-bold">
+            <Link className=" hover:text-amber-600">home</Link>
+            <Link className=" hover:text-amber-600">Instractors</Link>
+            <Link className=" hover:text-amber-600">Classes</Link>
+            <Link className=" hover:text-amber-600">Dashboard </Link>
+            </div>
+
+            <Link className=" font-bold hover:text-amber-600">Login </Link>
+            <div className="w-10 rounded-full">
+                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
+
+        </div>
+    );
+};
+
+export default Navbar;
